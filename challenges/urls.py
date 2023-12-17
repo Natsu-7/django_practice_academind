56b71d4ae9077c3355fa70ca1238a3067a7f917f
Chaitanya Shah <shahchaitanya2000@gmail.com>
2023-12-17 16:05:42 +0530

    Init reformat

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from . import views
+
 urlpatterns = [
-    path("",views.index),
-    path("<int:month>",views.month_challenges_by_number),
-    path("<str:month>",views.month_challenges,name="monthly_challenge"),
+    path("", views.index),
+    path("<int:month>", views.month_challenges_by_number),
+    path("<str:month>", views.month_challenges, name="monthly_challenge"),
 ]
